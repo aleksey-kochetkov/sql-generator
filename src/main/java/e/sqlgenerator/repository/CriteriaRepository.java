9c0ddf951e630b365d05d8879309dc1627dc1b19
akochetkov <akochetkov@ceki.ru>
2025-02-06 15:30:06 +0300

    jakarta.persistence.EntityManager

---
+++ src/main/java/e/sqlgenerator/repository/CriteriaRepository.java
@@ -0,0 +1,14 @@
+package e.sqlgenerator.repository;
+
+import jakarta.persistence.EntityManager;
+import org.springframework.stereotype.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Repository
+public class CriteriaRepository {
+    @Autowired
+    private EntityManager em;
+
+    public void run() {
+    }
+}
