7a0e7f3456a2060b5fb6d04544e705ab5bd24205
akochetkov <akochetkov@ceki.ru>
2025-03-09 16:30:44 +0300

    java.sql.ResultSet

--- src/main/java/e/sqlgenerator/sql/DatabaseMetaData.java
+++ src/main/java/e/sqlgenerator/sql/DatabaseMetaData.java
@@ -1,6 +1,18 @@
 package e.sqlgenerator.sql;
 
+import java.sql.RowIdLifetime;
+
 public class DatabaseMetaData implements java.sql.DatabaseMetaData {
+
+    @Override
+    public java.sql.ResultSet getClientInfoProperties() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public java.sql.Connection getConnection() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     @Override
     public boolean generatedKeyAlwaysReturned() {
@@ -8,12 +20,242 @@ public class DatabaseMetaData implements java.sql.DatabaseMetaData {
     }
 
     @Override
-    public ResultSet getPseudoColumns(String catalog, String schemaPattern, String tableNamePattern, String columnNamePattern) {
+    public RowIdLifetime getRowIdLifetime() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean autoCommitFailureClosesAllResultSets() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean supportsStoredFunctionsUsingCallSyntax() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean supportsStatementPooling() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean supportsSavepoints() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean supportsNamedParameters() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean supportsMultipleOpenResults() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean supportsGetGeneratedKeys() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean supportsResultSetHoldability(int holdability) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public int getResultSetHoldability() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean supportsResultSetConcurrency(int type, int concurrency) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean supportsResultSetType(int type) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean supportsBatchUpdates() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean insertsAreDetected(int type) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean updatesAreDetected(int type) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean deletesAreDetected(int type) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean ownInsertsAreVisible(int type) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean ownUpdatesAreVisible(int type) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean ownDeletesAreVisible(int type) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean othersInsertsAreVisible(int type) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean othersUpdatesAreVisible(int type) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean othersDeletesAreVisible(int type) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public int getDatabaseMajorVersion() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public int getDatabaseMinorVersion() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public int getJDBCMajorVersion() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public int getJDBCMinorVersion() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public int getSQLStateType() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean locatorsUpdateCopy() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public java.sql.ResultSet getSchemas() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public java.sql.ResultSet getSchemas(String catalog, String schemaPattern) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public java.sql.ResultSet getUDTs(String catalog, String schemaPattern, String typeNamePattern, int[] types) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public java.sql.ResultSet getSuperTypes(String catalog, String schemaPattern, String typeNamePattern) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public java.sql.ResultSet getSuperTables(String catalog, String schemaPattern, String tableNamePattern) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public ResultSet getFunctions(String catalog, String schemaPattern, String functionNamePattern) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public java.sql.ResultSet getColumns(String catalog, String schemaPattern, String tableNamePattern, String columnNamePattern) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public java.sql.ResultSet getPseudoColumns(String catalog, String schemaPattern, String tableNamePattern, String columnNamePattern) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public java.sql.ResultSet getFunctionColumns(String catalog, String schemaPattern, String functionNamePattern, String columnNamePattern) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public java.sql.ResultSet getVersionColumns(String catalog, String schema, String table) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public java.sql.ResultSet getBestRowIdentifier(String catalog, String schema, String table, int scope, boolean nullable) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public java.sql.ResultSet getIndexInfo(String catalog, String schema, String table, boolean unique, boolean approximate) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public java.sql.ResultSet getCrossReference(String parentCatalog, String parentSchema, String parentTable, String foreignCatalog, String foreignSchema, String foreignTable) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public java.sql.ResultSet getPrimaryKeys(String catalog, String schema, String table) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public java.sql.ResultSet getExportedKeys(String catalog, String schema, String table) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public java.sql.ResultSet getImportedKeys(String catalog, String schema, String table) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public java.sql.ResultSet getTablePrivileges(String catalog, String schemaPattern, String tableNamePattern) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public java.sql.ResultSet getColumnPrivileges(String catalog, String schema, String table, String columnNamePattern) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public java.sql.ResultSet getTypeInfo() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public java.sql.ResultSet getAttributes(String catalog, String schemaPattern, String typeNamePattern, String attributeNamePattern) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
