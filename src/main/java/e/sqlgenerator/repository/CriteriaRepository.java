3e0eaec2c6897b705fb2f6e81d8a74f491d8e6b6
akochetkov <akochetkov@ceki.ru>
2025-05-06 20:44:52 +0300

    jakarta.persistence.TypedQuery

--- src/main/java/e/sqlgenerator/repository/CriteriaRepository.java
+++ src/main/java/e/sqlgenerator/repository/CriteriaRepository.java
@@ -1,6 +1,7 @@
 package e.sqlgenerator.repository;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import org.springframework.stereotype.Repository;
@@ -15,5 +16,6 @@ public class CriteriaRepository {
     public void run() {
         CriteriaBuilder builder = em.getCriteriaBuilder();
         CriteriaQuery<Item> criteria = builder.createQuery(Item.class);
+        TypedQuery<Item> query = em.createQuery(criteria);
     }
 }
