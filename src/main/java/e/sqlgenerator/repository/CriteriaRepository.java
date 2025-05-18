db2c3557ac6048fc98571b085b1f01b7137be20a
akochetkov <akochetkov@ceki.ru>
2025-05-18 18:24:01 +0300

    jakarta.persistence.criteria.Root

--- src/main/java/e/sqlgenerator/repository/CriteriaRepository.java
+++ src/main/java/e/sqlgenerator/repository/CriteriaRepository.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import org.springframework.stereotype.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import e.sqlgenerator.model.Item;
@@ -16,7 +17,7 @@ public class CriteriaRepository {
     public void run() {
         CriteriaBuilder builder = em.getCriteriaBuilder();
         CriteriaQuery<Item> criteria = builder.createQuery(Item.class);
-        criteria.from(Item.class);
+        Root<Item> item = criteria.from(Item.class);
         TypedQuery<Item> query = em.createQuery(criteria);
         query.getResultList();
     }
